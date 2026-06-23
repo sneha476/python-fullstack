3b9da067bd6d776c03f35c18838bc0789ea667a7
Sneha Latha <athotasneha474@gmail.com>
2026-06-23 14:43:45 +0530

    Update leetcode231.py

--- leetcode231.py
+++ leetcode231.py
@@ -15,5 +15,17 @@ else:
 s=input("enter a character")
 if '0'<=s<='9':
      print(ord(s),s)
+    
+### leetcode236:##
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n%3==0:
+            n//=3
+        return n==1
+
+        
 
 
