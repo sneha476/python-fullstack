35ced9e1cf9a525f89e7905f3a84cc3bc4de26e7
Sneha Latha <athotasneha474@gmail.com>
2026-06-24 14:24:36 +0530

    Create basicsprograms.py

---
+++ basicsprograms.py
@@ -0,0 +1,21 @@
+# Sum of Digits 1234 → 10
+n=1234
+sum=0
+while n>0:
+    sum+=n%10
+    n//=10
+print(sum)
+# Reverse a Number    1234 → 4321
+n=4321
+rev=0
+while n>0:
+  m=n%10
+  rev=rev*10+m
+  n//=10
+print(rev)
+# Count Digits in a Number       12345 → 5  
+n=12345
+count=0
+for i in range(len(n)):
+  if n(i)=1
+  count+=1
